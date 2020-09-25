baff77b30cf22fdac6b9f6f289b5d00a1bbbd4d8
strozw <sxtxrxoxzxw@gmail.com>
2020-09-25 20:56:44 +0900

    remove force-enabled-speedy

--- workspaces/force-enabled-speedy/src/GlobalStyle.js
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components-force-enabled-speedy'
-
-const rules = Array.from(Array(3000).keys())
-	 .map(n => `.fs${n} { font-size: ${n}px; }`)
-	 .join("\n")
-
-export const GlobalStyle = createGlobalStyle`
-	 ${rules}
-`
